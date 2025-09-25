fb5ae8952e7b3e17e09e8a2c18f444d292d5c4be
Mohamed Amine Nafia <m.amine.nafia@gmail.com>
2025-09-25 13:50:48 +0200

    change the notification design for mobile view

--- weather-app/src/components/NotificationMessage.jsx
+++ weather-app/src/components/NotificationMessage.jsx
@@ -3,15 +3,20 @@ import errorIcon from "../assets/error.png";
 function NotificationMessage() {
   const { setNotification } = useNotification();
   return (
-    <div className="bg-black/10 backdrop-blur-2xl lg:py-6 lg:px-10 absolute flex items-center max-w-5/6 py-5 px-6   text-center  flex-col left-1/2 top-1/2 -translate-y-1/2 -translate-x-1/2">
-      <img width="54px" src={errorIcon} alt="error" />
+    <div className="bg-black/10 backdrop-blur-2xl lg:py-6 lg:px-10 absolute flex items-center max-w-6/7 py-5 px-4   text-center  flex-col left-1/2 top-1/2 -translate-y-1/2 -translate-x-1/2">
+      <img
+        className="lg:w-12 w-9
+      "
+        src={errorIcon}
+        alt="error"
+      />
       <h2 className="text-xl w-64 font-medium my-3">Something Went Wrong!</h2>
       <p className="mb-6 w-80 text-md text-center text-black">
         Enter a city or pick one from the suggestions below 🌍
       </p>
       <button
         onClick={() => setNotification(false)}
-        className="bg-white w-full p-2.5 text-lg font-medium text-black hover:bg-white/85"
+        className="bg-white w-full lg:p-2.5 text-lg p-1 font-medium text-black hover:bg-white/85"
       >
         Ok
       </button>
